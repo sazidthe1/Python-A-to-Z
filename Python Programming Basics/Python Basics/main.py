59058d88989d824b292b57574bfcbe46d3b0da93
Sazidul Islam <sazidthe1@gmail.com>
2023-11-03 13:46:05 +0000

    Add main.py in Python Basics

---
+++ Python Programming Basics/Python Basics/main.py
@@ -0,0 +1,10 @@
+# Tip Calculator App
+food_amount = float(input('Enter food amount $: '))
+tip_percentage =  float(input('Enter your tip percentage %: ')) / 100
+tip_amount = food_amount * tip_percentage # Getting the tip amount
+total = food_amount + tip_amount # Getting the total amount
+print('--------------------------------')
+print(f'🍜 Food Amount ${food_amount}')
+print(f'📝 Tip Amount: ${tip_amount}')
+print(f'💵 Total Amount: ${total}')
+print('--------------------------------')
