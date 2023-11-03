c5b203f0a0fe6c9529a671f30c0a885243a80c5f
Sazidul Islam <sazidthe1@gmail.com>
2023-11-03 15:09:46 +0000

    Adding weather.py

---
+++ Projects/Mini Projects/weather.py
@@ -0,0 +1,18 @@
+# Baby Weather App
+
+weather = input('What is the weather today? ')
+
+if weather == 'rain':
+    print('☔')
+
+elif weather == 'sunny':
+    print('☀️')
+
+elif weather == 'cloudy':
+    print('☁️')
+
+elif weather == 'thunderstorm':
+    print('⛈️')
+    
+else:
+    print('😐')
