447f528e6635b8e7f033571b01a68351ce7273c1
Sazidul Islam <sazidthe1@gmail.com>
2023-11-13 14:55:34 +0000

    Adding practice.py in Basics

---
+++ Programming Basics/practice.py
@@ -0,0 +1,100 @@
+# Operators
+'''
+Exponent: **
+Modulus/reminder: %
+Floor Division: //
+Division: /
+Multiplication: *
+Subtraction: -
+Addition: +
+'''
+2 + 3 * 6
+
+(2 + 3) * 6
+
+2 ** 8
+
+23 / 7
+
+23 // 7
+
+23 % 7
+
+(5 - 1) * ((7 + 1) / (3 - 1))
+
+
+# Data type
+'''
+Integers: -2, -1, 0, 1, 2, 3, 4, 5
+Floats: -1.25, -1.0, -0.05, 0.5, 1.0, 1.25
+Strings: 'a', 'Hello', '11 cats', 'Bangladesh'
+'''
+
+
+# Sring concatenation and replication
+'Alice' + 'Bob'
+
+'Alice' * 5
+
+
+# Storing values in Variables
+store = 40
+
+eggs = 2
+
+store + eggs + store
+
+store = store + 2
+
+text = 'Hello'
+
+text = 'Goodbye'
+
+# The str(), int(), and float() functions
+str(29)
+
+print('I am ' + str(29) + ' years old.')
+
+str(3.14)
+
+int('42')
+
+int(1.99)
+
+float(10)
+
+float(3.14)
+
+text = int(input('Enter your number: ')) # Using  int() & input() functions
+
+print(text * 10 / 5)
+
+
+# Text and Number equivalence
+42 == '42'
+
+42 == 42.0
+
+42.0 == 0042.000
+
+
+# Practice Questions
+a = 'spam' + 'spamspam'
+b = 'spam' * 3
+a == b
+
+'''
+3 functions to get integer, floating-point number, string
+- int()
+- float()
+- string()
+'''
+
+'''
+len(['1', '2', '3', '4']))
+
+round(2.495, 1))
+
+'''
+
+print('I have eaten ' + str(9) + ' bananas.')
