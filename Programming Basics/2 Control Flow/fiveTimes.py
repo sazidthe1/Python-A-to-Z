d7a880ca6baf04a9b5bb921060f4e3b00f77fb09
Sazidul Islam <sazidthe1@gmail.com>
2023-11-15 14:47:17 +0000

    Updating fiveTimes.py

--- Programming Basics/2 Control Flow/fiveTimes.py
+++ Programming Basics/2 Control Flow/fiveTimes.py
@@ -1,4 +1,13 @@
 # My name five times
 print('My name is')
-for i in range(5):
+for i in range(1, 6):
     print('Sazid (' + str(i) + ')')
+
+print('\n')
+
+# An equivalent while loop - my name five times
+print('My name is')
+i = 1
+while i < 6:
+    print('Sazid (' + str(i) + ')')
+    i += 1
