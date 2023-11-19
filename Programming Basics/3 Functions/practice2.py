dd73304cbd74c8b4463e51ca12f0a0ba15dccb08
Sazidul Islam <sazidthe1@gmail.com>
2023-11-19 16:21:07 +0000

    Add practice2.py & magic8Ball.py

---
+++ Programming Basics/3 Functions/practice2.py
@@ -0,0 +1,25 @@
+# Functions
+'''
+built-in functions = print(), input(), len()
+coutom functions = created by myself
+'''
+# Defining the fuunction
+def hello():
+    print('Hi!')
+    print('Hello.')
+    print('Hi there!')
+
+hello()     # Calling the function
+
+
+# def Statements with Parameters
+def hello(name):
+    print(f'Hello, {name}!')
+
+hello('World')
+hello('Sky')
+
+# Define, Call, Pass, Argument, Parameter
+def sayHello(name):
+    print(f'Hello, {name}!')
+sayHello('Sazid')
