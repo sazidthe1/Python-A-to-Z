0d921181b0decafc0b2a0629072055163902c84e
Sazidul Islam <sazidthe1@gmail.com>
2023-12-01 12:52:03 +0000

    Add practice3.py

---
+++ Programming Basics/4 Lists/practice3.py
@@ -0,0 +1,26 @@
+# The Lists data type
+'''
+[1, 2, 3]
+['cat', 'bat', 'rat', 'elephant']
+['hello', 3.1415, True, None, 42]
+freedom = ['Bangladesh', 16, 'December', 1971]
+'''
+
+'''
+text = ['I', 'love', 'my', 'country']
+print(text[1])
+print(text[1:])
+print(text[:1])
+'''
+
+'''
+message = [['Bangladesh', 'is', 'my', 'homeland'], ['I', 'love', 'homeland']]
+print(message[1][0])
+print(message[1][1])
+print(message[0][0])
+print(message[0][0])
+print(message[0][1])
+print(message[0][2])
+print(message[1][1])
+'''
+
