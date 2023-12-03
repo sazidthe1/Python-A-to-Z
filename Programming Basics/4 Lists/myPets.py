41fcba2667be9a880248bca6c64527032a5c92b1
Sazidul Islam <sazidthe1@gmail.com>
2023-12-03 15:54:05 +0000

    Update practice3.py & myPets.py

---
+++ Programming Basics/4 Lists/myPets.py
@@ -0,0 +1,9 @@
+# This is a new version
+
+myPets = ['Zophie', 'Pooka', 'Nocila']
+print('Enter a pet name:')
+name = input()
+if name not in myPets:
+    print('I do not have a pet named ' + name)
+else:
+    print(name + ' is my pet.')
--- Programming Basics/4 Lists/practice3.py
+++ Programming Basics/4 Lists/practice3.py
@@ -1,5 +1,5 @@
-# The Lists data type
 '''
+# The Lists data type
 [1, 2, 3]
 ['cat', 'bat', 'rat', 'elephant']
 ['hello', 3.1415, True, None, 42]
@@ -24,3 +24,73 @@ print(message[0][2])
 print(message[1][1])
 '''
 
+'''
+# Negative Indexes
+animal = ['cat', 'bat', 'rat', 'elephant']
+print(animal[-1])
+print(animal[-3])
+'''
+
+'''
+# Getting a List from Another List with Slices
+animal = ['cat', 'bat', 'rat', 'elephant']
+print(animal[0:3])
+print(animal[1:3])
+print(animal[0:-1])
+print(animal[:2])
+print(animal[1:])
+print(animal[:])
+'''
+
+'''
+# Getting a List's Length with the len() Function
+animal = ['cat', 'bat', 'rat', 'elephant']
+print(len(animal))
+'''
+
+'''
+# Changing Values in a List with Indexes
+animal = ['cat', 'bat', 'rat', 'elephant']
+animal[1] = 'cow'
+print(animal)
+animal[2] = animal[1]
+animal[-1] = 911
+print(animal)
+'''
+
+'''
+# List Concatenation and List Replication
+merged = [1, 2, 3] + ['A', 'B', 'C']
+print(merged)
+print(['A', 'B', 'C'] * 3)
+group1 = [1, 2, 3]
+group2 = group1 + ['A', 'B', 'C']
+print(group2)
+'''
+
+'''
+# Removing Values from Lists wuth del Statements
+animal = ['cat', 'bat', 'rat', 'elephant']
+del animal[3]
+print(animal)
+del animal[2]
+print(animal)
+
+# Working with Lists
+catName1 = 'Zophie'
+catName2 = 'Pooka'
+catName3 = 'Simon'
+catName4 = 'Lady'
+catName5 = 'Nocila'
+catName6 = 'Celo'
+'''
+
+'''
+# Using for Loops with Lists
+for i in range(4):
+    print(i)
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+'''
