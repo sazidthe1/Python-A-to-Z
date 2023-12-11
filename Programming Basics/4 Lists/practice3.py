d95ecffaa14081626f7df54e6445d4c807fc52eb
Sazidul Islam <sazidthe1@gmail.com>
2023-12-11 21:53:56 +0600

    Update practice3.py

--- Programming Basics/4 Lists/practice3.py
+++ Programming Basics/4 Lists/practice3.py
@@ -4,16 +4,12 @@
 ['cat', 'bat', 'rat', 'elephant']
 ['hello', 3.1415, True, None, 42]
 freedom = ['Bangladesh', 16, 'December', 1971]
-'''
 
-'''
 text = ['I', 'love', 'my', 'country']
 print(text[1])
 print(text[1:])
 print(text[:1])
-'''
 
-'''
 message = [['Bangladesh', 'is', 'my', 'homeland'], ['I', 'love', 'homeland']]
 print(message[1][0])
 print(message[1][1])
@@ -22,16 +18,12 @@ print(message[0][0])
 print(message[0][1])
 print(message[0][2])
 print(message[1][1])
-'''
 
-'''
 # Negative Indexes
 animal = ['cat', 'bat', 'rat', 'elephant']
 print(animal[-1])
 print(animal[-3])
-'''
 
-'''
 # Getting a List from Another List with Slices
 animal = ['cat', 'bat', 'rat', 'elephant']
 print(animal[0:3])
@@ -40,15 +32,11 @@ print(animal[0:-1])
 print(animal[:2])
 print(animal[1:])
 print(animal[:])
-'''
 
-'''
 # Getting a List's Length with the len() Function
 animal = ['cat', 'bat', 'rat', 'elephant']
 print(len(animal))
-'''
 
-'''
 # Changing Values in a List with Indexes
 animal = ['cat', 'bat', 'rat', 'elephant']
 animal[1] = 'cow'
@@ -56,9 +44,7 @@ print(animal)
 animal[2] = animal[1]
 animal[-1] = 911
 print(animal)
-'''
 
-'''
 # List Concatenation and List Replication
 merged = [1, 2, 3] + ['A', 'B', 'C']
 print(merged)
@@ -66,10 +52,8 @@ print(['A', 'B', 'C'] * 3)
 group1 = [1, 2, 3]
 group2 = group1 + ['A', 'B', 'C']
 print(group2)
-'''
 
-'''
-# Removing Values from Lists wuth del Statements
+# Removing Values from Lists with del Statements
 animal = ['cat', 'bat', 'rat', 'elephant']
 del animal[3]
 print(animal)
@@ -83,9 +67,7 @@ catName3 = 'Simon'
 catName4 = 'Lady'
 catName5 = 'Nocila'
 catName6 = 'Celo'
-'''
 
-'''
 # Using for Loops with Lists
 for i in range(4):
     print(i)
@@ -93,9 +75,7 @@ for i in range(4):
 supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
 for i in range(len(supplies)):
     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
-'''
 
-'''
 # Using the random.choice() with Lists
 import random
 pets = ['Bat', 'Cat', 'Rat']
@@ -106,9 +86,7 @@ import random
 people = ['Alice', 'Bob', 'Carol', 'David']
 random.shuffle(people)
 print(people)
-'''
 
-'''
 # Methods
 # Finding a Value in a List with the index() Method
 greetings = ['hello', 'hi', 'howdy', 'heyas']
@@ -139,9 +117,7 @@ print(number)
 animal = ['bat', 'cat', 'rat']
 animal.reverse()
 print(animal)
-'''
 
-'''
 # Sequence Data Types
 name = 'Zophie'
 #print(name[0])
@@ -154,9 +130,7 @@ name = 'Zophie is a cat.'
 article = 'the'
 newName = name[:10] + article + name[11:]
 print(newName)
-'''
 
-'''
 # The Tuple Data Type
 eggs = ('hello', 42, 0.5)
 print(eggs[0])
@@ -174,9 +148,7 @@ spam = [0, 1, 2, 3, 4, 5]
 cheese = spam
 cheese[0] = 'hello'
 print(spam)
-'''
 
-'''
 # Identity and the id() Function
 print(id('Hi'))
 print(id('Hello'))
@@ -186,8 +158,7 @@ eggs = ['bat', 'cat']
 print(id(eggs))
 eggs.append('rat')
 print(id(eggs))
-'''
-'''
+
 # Passing References
 def eggs(someParameter):
     someParameter.insert(0, 'Hello')
