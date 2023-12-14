964b8d7326a64c8a78b2e1a523a35e958b6fd570
Sazidul Islam <sazidthe1@gmail.com>
2023-12-14 10:03:30 +0000

    Update characterPicture.py

--- Programming Basics/4 Lists/characterPicture.py
+++ Programming Basics/4 Lists/characterPicture.py
@@ -11,13 +11,13 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
 # Get the dimensions of the grid
-width = len(grid)
-height = len(grid[0])
+height = len(grid)
+width = len(grid[0])
 
 # Loop through each row
-for y in range(height):
+for y in range(width):
     # Loop through each column
-    for x in range(width):
+    for x in range(height):
         # Print the character at the current position, followed by a space
         print(grid[x][y], end='')
     # Print a new line after each row is printed
