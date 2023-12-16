4b5b05bed0e0866aefc8940c090742d739c0f887
Sazidul Islam <sazidthe1@gmail.com>
2023-12-16 17:45:37 +0000

    Update practice4.py

--- Programming Basics/5 Dictionaries/practice4.py
+++ Programming Basics/5 Dictionaries/practice4.py
@@ -1,2 +1,12 @@
 # This is for practice
 
+myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
+
+print(type(myCat))
+print(len(myCat))
+
+# key: size, color, disposition
+# value: fat, gray, loud
+
+print(myCat['size'])    # Accessing the value
+print('My cat has ' + myCat['color'] + ' fur.')
