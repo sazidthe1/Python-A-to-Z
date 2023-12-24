c95a6e3a02eb0a6670ef175781123737e860d168
Sazidul Islam <sazidthe1@gmail.com>
2023-12-24 17:48:27 +0000

    Update practice5.py

--- Programming Basics/6 Regex/practice5.py
+++ Programming Basics/6 Regex/practice5.py
@@ -1,6 +1,6 @@
 '''
 # This is the practice code
-'''
+
 # Finding Patterns of Text Without Regular Expressions
 from phoneNumber import isPhoneNumber
 
@@ -10,3 +10,48 @@ for i in range(len(message)):
     if isPhoneNumber(chunk):
         print('Phone number found: ' + chunk)
 print('Done')
+
+
+# Finding Patterns of Text with Regular Expressions
+# Creating Regex Objects
+import re
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 415-555-4242')  # Matching Regex Objects
+print('Phone number found: ' + mo.group())
+
+# Grouping with Parentheses
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(0))
+print(mo.group())
+
+phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My phone number is (415) 555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(0))
+print(mo.group())
+
+
+# Matching Multiple Groups with the Pipe
+heroRegex = re.compile (r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+print(mo1.group())
+mo2 = heroRegex.search('Tina Fey and Batman')
+print(mo2.group())
+
+import re
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group)
+print(mo.group(1))
+
+# Optional Matching with the Question Mark
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+'''
