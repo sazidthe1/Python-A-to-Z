26800acd31169313b3bfc54e6240f04e03a75f9b
Sazidul Islam <sazidthe1@gmail.com>
2023-12-25 11:28:17 +0000

    Update practice5.py

--- Programming Basics/6 Regex/practice5.py
+++ Programming Basics/6 Regex/practice5.py
@@ -54,4 +54,71 @@ mo1 = batRegex.search('The Adventures of Batman')
 print(mo1.group())
 mo2 = batRegex.search('The Adventures of Batwoman')
 print(mo2.group())
+
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+mo1 = phoneRegex.search('My number is 415-555-4242')
+print(mo1.group())
+mo2 = phoneRegex.search('My number is 555-4242')
+print(mo2.group())
+
+# Matching Zero or More with the Star
+batRegex = re.compile(r'Bat(wo)*man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+mo3 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo3.group())
+
+# Matching One or More with the Plus
+batRegex = re.compile(r'Bat(wo)+man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo2.group())
+mo3 = batRegex.search('The Adventures of Batman')
+print(mo3 == None)
+
+# Matching Specific Repetitions with Braces
+haRegex = re.compile(r'(Ha){3}')
+mo1 = haRegex.search('HaHaHa')
+print(mo1.group())
+mo2 = haRegex.search('Ha')
+print(mo2 == None)
+
+# Greedy and Non-greedy Matching
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo2.group())
+
+# The findall() Method
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('Cell: 415-555-9999; Work: 212-555-0000')
+print(mo.group())
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # has no groups
+mo1 = phoneNumRegex.findall('Cell: 415-555-9999; Work: 212-555-0000')
+print(mo1)
+
+
+# Character Classes
+# Shorthand character class: Represents
+
+\d: Any numeric digit from 0 to 9.
+
+\D: Any character that is not a numeric digit from 0 to 9.
+
+\w: Any letter, numeric digit, or the underscore character. (Think of this as matching “word” characters.)
+
+\W: Any character that is not a letter, numeric digit, or the underscore character.
+
+\s: Any space, tab, or newline character. (Think of this as matching “space” characters.)
+
+\S: Any character that is not a space, tab, or newline.
+
+shoppingRegex = re.compile(r'\d+\s\w+')
+mo2 = shoppingRegex.findall('7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 chicken')
+print(mo2)
 '''
