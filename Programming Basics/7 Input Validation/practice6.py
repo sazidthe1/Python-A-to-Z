f2cfd6a87536b03a30b4e3705ab2cd5133eb7526
Sazidul Islam <sazidthe1@gmail.com>
2024-01-08 17:55:58 +0000

    Add practice6.py

--- Programming Basics/7 Input Validation/practice6.py
+++ Programming Basics/7 Input Validation/practice6.py
@@ -38,3 +38,44 @@ print(response)
 response = pyip.inputNum(blank=True)    # blank is True
 print(response)
 '''
+
+# The limit, timeout, and default Keyword Arguments
+import pyinputplus as pyip
+'''
+response = pyip.inputNum('Enter num: ', limit=2)
+print(response)
+
+response = pyip.inputNum(timeout=10)
+print(response)
+
+response = pyip.inputNum(limit=3, default='N/A')    # using default keyword argument
+print(response)
+
+# The allowRegexes and blockRegexes Keyword Arguments
+
+import pyinputplus as pyip
+
+#response = pyip.inputNum(allowRegexes=[r'(I|V|X|L|C|D|M|i|v|x|l|c|d|m)+', r'zero'])   # allowing input
+#print(response)
+
+#response = pyip.inputNum(blockRegexes=[r'[02468]$'])  # blocking input
+#print(response)
+
+response = pyip.inputStr(allowRegexes=[r'caterpillar', 'category'],
+blockRegexes=[r'bat', 'cat', 'hat', 'rat'])
+print(response)
+
+# Passing a Custom Validation Function to inputCustom()
+import pyinputplus as pyip
+
+def addToTen(numbers):
+    numbersList = list(numbers)
+    for i, digit in enumerate(numbersList):  # Fixed syntax error here
+        numbersList[i] = int(digit)
+    if sum(numbersList) != 10:
+        raise Exception('The digits must add up to 10, not %s.' % sum(numbersList))
+    return int(numbers)
+
+response = pyip.inputCustom(addToTen)
+print(response)
+'''
