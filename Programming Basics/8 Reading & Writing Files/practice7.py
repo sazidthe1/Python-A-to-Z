484949fc102a7706c678a34306fdbb5b2bf77172
Sazidul Islam <sazidthe1@gmail.com>
2024-01-12 14:42:54 +0000

    Update practice7.py

--- Programming Basics/8 Reading & Writing Files/practice7.py
+++ Programming Basics/8 Reading & Writing Files/practice7.py
@@ -4,7 +4,7 @@
 # Files and File Paths
 A file has two key properties: a filename and a path. 
 The path specifies the location of a file on the computer. For example, project.docx in the path C:\Users\Al\Documents.
-'''
+
 from pathlib import Path
 myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
 for filename in myFiles:
@@ -14,3 +14,25 @@ for filename in myFiles:
 # C:\Users\Al\accounts.txt
 # C:\Users\Al\details.csv
 # C:\Users\Al\invite.docx
+    
+# Using the / Operator to Join Paths
+homeFolder = r'C:\Users\Al'
+subFolder = 'spam'
+homeFolder + '\\' + subFolder
+'\\'.join([homeFolder, subFolder])
+
+homeFolder = Path('C:/Users/Al')
+subFolder = Path('spam')
+homeFolder / subFolder
+
+# The Current Working Directory
+from pathlib import Path
+import os
+Path.cwd()  # Output: WindowsPath('C:/Users/Al/AppData/Local/Programs/Python/Python37')'
+
+os.chdir('C:\\Windows\\System32')
+Path.cwd()  # Output: WindowsPath('C:/Windows/System32')
+
+# The Home Directory
+Path.home() # Output: WindowsPath('C:/Users/Al')
+'''
